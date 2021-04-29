500c2069ec43bd2853a101905277da002fcedebd
Lenin Fernando Rugeles Muñoz - Ceiba Software <fernando.rugeles@ceiba.com.co>
2021-04-29 13:37:09 -0500

    ajustes

--- src/app/feature/cliente/components/verificar-credenciales-cliente/verificar-cliente.spec.ts
+++ src/app/feature/cliente/components/verificar-credenciales-cliente/verificar-cliente.spec.ts
@@ -13,6 +13,7 @@ import { Cliente } from '@cliente/shared/model/cliente';
 import { of } from 'rxjs';
 import { Cuenta } from '@cuenta/shared/model/cuenta';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Transaccion } from '@transaccion/shared/model/transaccion';
 
 describe('VerificarCredencialesClienteComponent', () => {
   let component: VerificarCredencialesClienteComponent;
@@ -81,6 +82,23 @@ describe('VerificarCredencialesClienteComponent', () => {
       // assert
       expect(component.cliente).toEqual(cliente);
     });
+
+    it('debe retornar cuentas para la reocnstruccion', () => {
+      // arrange
+      const cuenta = new Cuenta(1, '1234567890', 1200000, 500000, 1, new Date());
+      const cuenta2 = new Cuenta(2, '1234567891', 1200000, 500000, 1, new Date());
+      const cliente = new Cliente(1, 'fernando', 'rugeles', 1, '1098744055', '2021-04-15 09:55:43', 1);
+      const listadoCuentas = [cuenta, cuenta2];
+      component.notificacion = notificacionService;
+      component.cliente = cliente;
+      spyOn(clienteService, 'obtenerListaCuentaSegunCliente').and.returnValue(of(listadoCuentas));
+
+      // act
+      component.consultarCuentasSegunCliente();
+
+      // assert
+      expect(component.cliente.listadoCuentas.length).toBeGreaterThan(0);
+    });
   });
 
   describe('cuando el metodo consultarCuentasSegunCliente es llamado', () => {
@@ -99,6 +117,58 @@ describe('VerificarCredencialesClienteComponent', () => {
 
       // assert
       expect(component.cliente.listadoCuentas.length).toBeGreaterThan(0);
+    });
+  });
+
+
+  describe('cuando el metodo consultarTrancaccion es llamado', () => {
+    it('debe retornar una lista de transacciones', () => {
+      // arrange
+      const cuenta = new Cuenta(1, '1234567890', 1200000, 500000, 1, new Date());
+      const listaTransacciones = [
+        new Transaccion(1, 1, 2, 11200, 0.5, new Date(), 1, '', ''),
+        new Transaccion(2, 1, 2, 200, 0.5, new Date(), 1, '', '')
+      ];
+      component.notificacion = notificacionService;
+      spyOn(clienteService, 'obtenerListaTransaccionPorCuenta').and.returnValue(of(listaTransacciones));
+
+      // act
+      component.consultarTrancaccion(cuenta);
+
+      // assert
+      expect(component.listadoDeTransacciones .length).toBeGreaterThan(0);
+    });
+
+    it('no debe retornar una lista de transacciones', () => {
+      // arrange
+      const cuenta = new Cuenta(1, '1234567890', 1200000, 500000, 1, new Date());
+      const listaTransacciones = [];
+      component.notificacion = notificacionService;
+      spyOn(clienteService, 'obtenerListaTransaccionPorCuenta').and.returnValue(of(listaTransacciones));
+
+      // act
+      component.consultarTrancaccion(cuenta);
+
+      // assert
+      expect(component.listadoDeTransacciones.length).toBe(0);
+    });
+  });
+
+
+  describe('cuando el metodo nuevaBusqueda es llamado', () => {
+    it('debe limpiar la variables', () => {
+      // arrange
+      const cliente = new Cliente(1, 'fernando', 'rugeles', 1, '1098744055', '2021-04-15 09:55:43', 1);
+      component.cliente = cliente;
+
+      // act
+      component.nuevaBusqueda();
+
+      // assert
+      expect(component.cliente).toBeUndefined();
+      expect(component.usuarioEncontrado).toBeFalsy();
+      expect(component.listadoDeCuentasDeCliente.length).toBe(0);
+      expect(component.listadoDeTransacciones.length).toBe(0);
     });
   });
 });
